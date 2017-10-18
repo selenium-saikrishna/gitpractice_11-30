a15d4605af7ace86a5e61d779f1b4da8bafbcc4a
Sai Krishna <selenium.saikrishna@gmail.com>
2017-10-18 00:22:35 -0700

    First Commit

---
+++ DropdownOnGrid.java
@@ -0,0 +1,58 @@
+package day5;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.server.browserlaunchers.Sleeper;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class DropdownOnGrid {
+	@Parameters("browser")
+	@Test
+	public void dropdownTest(String b) throws MalformedURLException
+	{
+		DesiredCapabilities cap=null;
+		if(b.equals("firefox"))
+		{
+			cap=DesiredCapabilities.firefox();
+			cap.setBrowserName("firefox");
+			cap.setPlatform(Platform.WINDOWS);
+		}
+		else if(b.equals("chrome"))
+		{
+			cap=DesiredCapabilities.chrome();
+			cap.setBrowserName("chrome");
+		    cap.setPlatform(Platform.MAC);
+		}
+		
+		RemoteWebDriver driver=new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),cap);
+		
+		driver.get("http://newtours.demoaut.com");
+		Sleeper.sleepTightInSeconds(5);
+		driver.findElement(By.linkText("REGISTER")).click();
+		WebElement drop=driver.findElement(By.name("country"));
+		List<WebElement> dropdown=drop.findElements(By.tagName("option"));
+		System.out.println(dropdown.size());
+		for(int i=0;i<20;i++)
+		{
+			dropdown.get(i).click();
+			if(dropdown.get(i).isSelected())
+			{
+				System.out.println(dropdown.get(i).getText()+" is active");
+			}
+			else
+			{
+				System.out.println(dropdown.get(i).getText()+" is inactive");
+			}
+		}
+	}
+
+
+}
